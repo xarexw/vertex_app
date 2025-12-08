aec9b896802a24495b6b8a5b9ca4e0cb0ae5bb70
xarexw <xarexw2939@protonmail.com>
2025-12-08 06:39:58 +0200

    feat(records): full live mode for trainer, summary and stats

--- apps/schedule/admin.py
+++ apps/schedule/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import TrainingSession
+from .models import TrainingSession, Record
 
 @admin.register(TrainingSession)
 class TrainingSessionAdmin(admin.ModelAdmin):
     list_display = ['title', 'trainer', 'start_time', 'end_time', 'capacity']
     list_filter = ['start_time', 'trainer']
+    
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['client', 'exercise_name', 'result_value', 'session', 'created_at']
+    list_filter = ['client', 'session']
--- apps/users/views.py
+++ apps/users/views.py
@@ -3,7 +3,21 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
+from .models import ClientStat
     
 @login_required
 def profile_view(request):
-    return render(request, 'users/profile.html', {'user': request.user})
+    stat, created = ClientStat.objects.get_or_create(user=request.user)
+    
+    # отримання останніх 5 записів тренувань
+    # 'training_records' - related_name, прописане в apps/schedule/models.py
+    recent_records = request.user.training_records.all().order_by('-created_at')[:5]
+    
+    context = {
+        'user': request.user,
+        'stat': stat,
+        'recent_records': recent_records
+    }
+    
+    return render(request, 'users/profile.html', context)
+    # return render(request, 'users/profile.html', {'user': request.user})
