2fa4d80b4bc4809b29bba717952b4ec45fbc3d62
xarexw <xarexw2939@protonmail.com>
2025-12-08 06:48:43 +0200

    fix(dashboard): different vievs for client/trainer

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,30 +2,47 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from subscriptions.models import UserSubscription
+from schedule.models import TrainingSession
 
 @login_required
 def dashboard(request):
     user = request.user
+    today = timezone.now().date()
+    
     context = {
         'user': user,
+        # дефолт пусті значення
         'has_active_sub': False,
         'days_left': 0,
         'sub_end_date': None,
-        'notifications': user.notifications.filter(is_read=False)[:3] # Останні 3 непрочитані
+        'notifications': [],
+        'trainer_sessions': [] # змінна для тренера
     }
 
-    # Шукаємо активну підписку
-    active_sub = UserSubscription.objects.filter(
-        user=user, 
-        is_active=True, 
-        end_date__gte=timezone.now().date()
-    ).first()
+    # ЛОГІКА ДЛЯ ТРЕНЕРА
+    if user.is_staff:
+        # тренування де є тренером
+        upcoming_sessions = TrainingSession.objects.filter(
+            trainer=user,
+            start_time__date__gte=today
+        ).order_by('start_time')[:5] # 5 найближчих
+        
+        context['trainer_sessions'] = upcoming_sessions
 
-    if active_sub:
-        context['has_active_sub'] = True
-        context['sub_end_date'] = active_sub.end_date
-        # Рахуємо дні до кінця
-        delta = active_sub.end_date - timezone.now().date()
-        context['days_left'] = max(0, delta.days)
+    # ЛОГІКА ДЛЯ КЛІЄНТА
+    else:
+
+        context['notifications'] = user.notifications.filter(is_read=False)[:3]
+        active_sub = UserSubscription.objects.filter(
+            user=user, 
+            is_active=True, 
+            end_date__gte=today
+        ).first()
+
+        if active_sub:
+            context['has_active_sub'] = True
+            context['sub_end_date'] = active_sub.end_date
+            delta = active_sub.end_date - today
+            context['days_left'] = max(0, delta.days)
 
     return render(request, 'core/dashboard.html', context)
