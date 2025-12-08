e5ef1772bd97debfa107201b6cd20f7b14067b31
xarexw <xarexw2939@protonmail.com>
2025-12-08 04:16:43 +0200

    fix(templates): import render / add dirs in settings

--- apps/subscriptions/urls.py
+++ apps/subscriptions/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import subscription_list
 
 urlpatterns = [
-    # Ось це ім'я 'subscription_list' шукає твій Dashboard
     path('', subscription_list, name='subscription_list'),
 ]
